ab3152b1fcd4985e39d81ca7b8439932b597c6b9
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2015-01-05 03:26:05 -0500

    clock ticking

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 function ready (){
 	
+	setInterval(updateClock, 1000);
+	updateClock();
 	
+}
+function zerocorrect(val){
+	if(val<10) {
+		return "0"+val;
+
+	}else{
+		return val;
+	}
+}
+function updateClock(){
+	var clock = new Date()
+	var element = document.getElementById('clock');
+	element.innerHTML = zerocorrect(clock.getHours())+":"+zerocorrect(clock.getMinutes())+":"+zerocorrect(clock.getSeconds())
 }
 window.onload = ready
