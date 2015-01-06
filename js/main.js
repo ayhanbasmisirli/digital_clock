3b107083c3f55df4eaa32e831ac6300924f83256
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2015-01-06 03:43:43 -0500

    adding prototype update and zerocorrect

--- js/main.js
+++ js/main.js
@@ -3,11 +3,6 @@ function ready (){
 	
 	var clock1 = new Clock('clock_element',120);
 	var clock2 = new Clock('clock_element2',-300,'Toronto');
-	
-	
-	
-
-
 }
 
 function Clock(id,offset,label){
@@ -16,25 +11,29 @@ function Clock(id,offset,label){
        	label = label || ' ';
        	var  d  = new Date();
  	this.offset = (offset+d.getTimezoneOffset())*60*1000;
- 	this.updateClock = function(){
- 		var date = new Date()
- 		date = new Date(this.offset+date.getTime());
-		var element = document.getElementById(id);
-		element.innerHTML =  this.zerocorrect(date.getHours())+":"+this.zerocorrect(date.getMinutes())+":"+this.zerocorrect(date.getSeconds())+' '+label
-		//console.log(this);
-		
- 	};
-
- 	this.zerocorrect =function(val){
- 		if(val<10) val = "0"+val;
-		return val;
-	};
+ 	this.id = id;
+ 	this.label =label;
+ 	
  	
  	var that = this; //function inside the set interval doesnt have access the object refering so keep the object on we refer variable to 
  	setInterval(function(){that.updateClock()}, 1000);
  	this.updateClock();
  	
 }
+
+Clock.prototype.updateClock = function(){
+ 		var date = new Date()
+ 		date = new Date(this.offset+date.getTime());
+		var element = document.getElementById(this.id);
+		element.innerHTML =  this.zerocorrect(date.getHours())+":"+this.zerocorrect(date.getMinutes())+":"+this.zerocorrect(date.getSeconds())+' '+this.label
+		//console.log(this);
+		
+ 	};
+
+Clock.prototype.zerocorrect =function(val){
+ 		if(val<10) val = "0"+val;
+		return val;
+};
 
 
 window.onload = ready
